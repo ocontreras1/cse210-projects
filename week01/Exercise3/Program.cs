898c135986e1a9c5017fc44aa4c7b94dc97c5588
ocontreras1 <ocontrerascruz@byupathway.edu>
2025-05-10 19:58:51 -0400

    Week 1 exercises

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
-    }
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 20);
+        int guessNumber = 0;
+        do 
+        {   
+            Console.Write("What is the magic number: ");
+            string guess = Console.ReadLine();
+
+            guessNumber = int.Parse(guess);
+        
+            if (guessNumber > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {   
+                Console.WriteLine("You guessed it!");
+            }
+        } while (guessNumber != magicNumber);
+    }   
 }
