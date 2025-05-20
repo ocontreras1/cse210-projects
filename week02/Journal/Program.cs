7134fb61fcb3828ffe91392b148044bc44360995
ocontreras1 <ocontrerascruz@byupathway.edu>
2025-05-19 23:44:48 -0400

    Updated Journal program.

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         int userChoice = 6;
+        List<Entry> journal = new List<Entry>();
 
         //Loop to continue showing the menu until user chooses to end the program
         while (userChoice != 0)
@@ -18,6 +20,7 @@ class Program
             Console.WriteLine("0.  Exit the journal");
             string userChoiceString = Console.ReadLine();
             userChoice = int.Parse(userChoiceString);
+            Entry entry1 = new Entry();
 
             // If statement that causes the program to execute each of of the options
             if (userChoice == 1)
@@ -25,7 +28,6 @@ class Program
                 //Generate a prompt, display it, and save the response from the user
                 PromptGenerator generator = new PromptGenerator();
                 string prompt = generator.GetRandomPrompt();
-                Entry entry1 = new Entry();
                 entry1._promptText = prompt;
                 Console.WriteLine(prompt);
                 entry1._entryText = Console.ReadLine();
@@ -33,13 +35,27 @@ class Program
                 //get the date and store as a string
                 DateTime theCurrentTime = DateTime.Now;
                 entry1._date = theCurrentTime.ToShortDateString();
+
+                //save entry to journal
+                journal.Add(entry1);
             }
 
             else if (userChoice == 2)
             {
-                entry1.Display();
-            }
+                if (journal.Count == 0)
+                {
+                    Console.WriteLine("Your journal is empty.");
+                }
+                else
+                {
+                    foreach (Entry entry in journal)
+                    {
+                        entry.Display();
+                    }
 
+                }
+            }
+            
             else if (userChoice == 3)
             {
                 Console.WriteLine("Choice 3");
@@ -60,7 +76,8 @@ class Program
                 Console.WriteLine("Please enter a valid choice.");
             }
 
+            }
+            //Console.WriteLine("Hello World! This is the Journal Project.");
         }
-        //Console.WriteLine("Hello World! This is the Journal Project.");
     }
 }
